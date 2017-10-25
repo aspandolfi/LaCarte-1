19347132109fec37e7fbf515454ada1d56e6d5c7
lukasg18 <lukas.gomes2010@gmail.com>
2017-10-25 21:53:45 -0200

    tab page

--- src/pages/cardapio-test/cardapio-test.ts
+++ src/pages/cardapio-test/cardapio-test.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, LoadingController } from 'ionic-angular';
 
 /**
  * Generated class for the CardapioTestPage tabs.
@@ -13,13 +13,38 @@ import { IonicPage, NavController } from 'ionic-angular';
   selector: 'page-cardapio-test',
   templateUrl: 'cardapio-test.html'
 })
-export class CardapioTestPage {
+export class CardapioTestPage implements OnInit {
+
+  tab: any;
+
+  ngOnInit(): void {
+
+  }
 
   comidasRoot = 'ComidasPage'
   bebidasRoot = 'BebidasPage'
   sobremesasRoot = 'SobremesasPage'
 
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+  }
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+    this.tab = this;
+  }
+
+  itens = [
+    { nome: "A", urlImage: "sssss", valor: 10 },
+    { nome: "A", urlImage: "sssss", valor: 10 },
+    { nome: "A", urlImage: "sssss", valor: 10 },
+    { nome: "A", urlImage: "sssss", valor: 10 },
+    { nome: "A", urlImage: "sssss", valor: 10 },
+    { nome: "A", urlImage: "sssss", valor: 10 },
+    { nome: "A", urlImage: "sssss", valor: 10 },
+  ]
 
 }
