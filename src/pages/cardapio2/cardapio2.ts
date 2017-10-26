7a328fa766e44c4db9ea7e5c15a46d62d0b7b6f8
luizlk <luiz.lk.lima@gmail.com>
2017-10-26 21:43:21 -0200

    dinamizando cardapio2

--- src/pages/cardapio2/cardapio2.ts
+++ src/pages/cardapio2/cardapio2.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Produto } from '../../class/produtos';
+import { CarrinhoPage } from '../carrinho/carrinho';
 
 @IonicPage()
 @Component({
   selector: 'page-cardapio2',
   templateUrl: 'cardapio2.html',
 })
+
 export class Cardapio2Page {
   public produto = new Produto();
   
-  Produto={"name": "Lasanha","simbolo": "R$", "valor": "16.00","url":"https://i.imgur.com/Q5ISx1U.jpg" ,"descricao":"Massa caseira, molho a bolonhesa, presunto, queijo mussarela, molho branco."};
+  Produto = {
+    "nome": "Lasanha", "valor": "16,00", "url": "https://i.imgur.com/Q5ISx1U.jpg", "descricao": "Massa caseira, molho a bolonhesa, presunto, queijo mussarela, molho branco."
+  };
+
+  adicionais = [
+    {"nome": "Bacon", "id": "1", "quantidade": 0},
+    {"nome": "Uva Passas", "id": "2", "quantidade": 0},
+    {"nome": "Cebola extra", "id": "3", "quantidade": 0}
+  ]
   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -18,8 +28,13 @@ export class Cardapio2Page {
   ionViewDidLoad() {
     console.log('ionViewDidLoad Cardapio2Page');
   }
+
+  public mudaQuantia(adic: any, val: number){
+    adic.quantidade += val;
+  }
+
   public moveTo(){ // uma simples função que esta sendo chamada pela primeira imagem do cardapio para poder visualizar a descrição da comida.
-    this.navCtrl.push(Cardapio2Page)
+    this.navCtrl.push(CarrinhoPage)
   }
 
 }
