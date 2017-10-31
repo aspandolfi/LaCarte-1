d508632b8dca38134521e09876324d90b145731c
lukasg18 <lukas.gomes@hotmail.com>
2017-10-31 21:13:08 -0200

    pagina de login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,16 +1,21 @@
-import { PerfilPage } from './../perfil/perfil';
-import { User } from './../../class/User';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
-import { RestProvider } from '../../providers/rest/rest';
-import { CadastPage } from '../cadast/cadast';
+import { PerfilPage } from "./../perfil/perfil";
+import { User } from "./../../class/User";
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController
+} from "ionic-angular";
+import { AlertController } from "ionic-angular";
+import { RestProvider } from "../../providers/rest/rest";
+import { CadastPage } from "../cadast/cadast";
 // import { SqliteServe } from '../../class/SqliteServe';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
   usuarios: string[];
@@ -19,10 +24,16 @@ export class LoginPage {
   userDetails: any;
   responseData: any;
   public usuarioLogin = new User();
-  public data1 = JSON.parse(localStorage.getItem('userData'))
-  userData = { "name": "", "email": "", "telefone": "", "cpf": "", "senha": "" }; // apenas pra teste
+  public data1: any;
+  userData = { name: "", email: "", telefone: "", cpf: "", senha: "" }; // apenas pra teste
 
-  constructor(public loadingCtrl: LoadingController,public navCtrl: NavController, public navParams: NavParams, public rest: RestProvider, public alertCtrl: AlertController) {
+  constructor(
+    public loadingCtrl: LoadingController,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public rest: RestProvider,
+    public alertCtrl: AlertController
+  ) {
     this.usuarioLogin.email = "";
     this.usuarioLogin.senha = "";
   }
@@ -32,68 +43,68 @@ export class LoginPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    console.log("ionViewDidLoad LoginPage");
   }
 
-  Validar() { //verifica se o usuario se encontra no banco.
+  Validar() {
+    //verifica se o usuario se encontra no banco.
     let loading = this.loadingCtrl.create({
-      content: 'Fetching content...'
+      content: "Fetching content..."
     });
-
-    if (this.usuarioLogin.email === "") {
-      console.log(this.usuarioLogin.email)
+    loading.present();
+    if (this.usuarioLogin.email === "" || this.usuarioLogin.senha === "") {
+      loading.dismiss();
       this.showAlert();
     } else {
+      console.log(this.rest.getUserEmail(this.usuarioLogin.email));
       this.rest.getUserEmail(this.usuarioLogin.email).subscribe(data => {
-        loading.present();
-        console.log(data);
         this.user = data;
+        localStorage.setItem("userData", JSON.stringify(this.user));
+        this.data1 = JSON.parse(localStorage.getItem("userData"));
         loading.dismiss();
-        localStorage.setItem('userData', JSON.stringify(this.user))
-        console.log(localStorage);
-      }
-      );
-
-      if (this.usuarioLogin.email === this.data1.email && this.usuarioLogin.senha === this.data1.senha) {
-        this.showAlertOn();
-        this.navCtrl.push(PerfilPage)
-      } else {
-        this.showAlert();
-      }
+        if (
+          this.usuarioLogin.email === this.data1.email &&
+          this.usuarioLogin.senha === this.data1.senha
+        ) {
+          this.showAlertOn();
+          this.navCtrl.push(PerfilPage);
+        } else {
+          this.showAlert();
+        }
+      });
     }
   }
 
   getData() {
     this.rest.getUser(1).subscribe(data => {
-      console.log(data);
+      // console.log(data);
       this.user = data;
-      localStorage.setItem('userData', JSON.stringify(this.user))
-      console.log(localStorage);
-    }
-    );
+      localStorage.setItem("userData", JSON.stringify(this.user));
+      // console.log(localStorage);
+    });
   }
 
-  showAlert() { // alerta para erro de login
+  showAlert() {
+    // alerta para erro de login
     let alert = this.alertCtrl.create({
-      title: 'Erro',
-      subTitle: 'Não foi possivel logar, login ou senha incorreto!',
-      buttons: ['OK']
+      title: "Erro",
+      subTitle: "Não foi possivel logar, login ou senha incorreto!",
+      buttons: ["OK"]
     });
     alert.present();
   }
 
-  showAlertOn() { // alerta para inicio do uso do app
+  showAlertOn() {
+    // alerta para inicio do uso do app
     let alert = this.alertCtrl.create({
-      title: 'La Carte',
-      subTitle: 'Bem vindo ao La Carte! Desejamos uma boa refeição!',
-      buttons: ['OK']
+      title: "La Carte",
+      subTitle: "Bem vindo ao La Carte! Desejamos uma boa refeição!",
+      buttons: ["OK"]
     });
     alert.present();
-
   }
 
   public moveTo() {
     this.navCtrl.push(CadastPage);
   }
-
 }
