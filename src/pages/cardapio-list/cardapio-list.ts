db0495e79d9334e8cbb6390fd582e82089a28b0f
lukasg18 <lukas.gomes@hotmail.com>
2017-11-07 13:22:24 -0200

    search bar v.1

--- src/pages/cardapio-list/cardapio-list.ts
+++ src/pages/cardapio-list/cardapio-list.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Cardapio2Page } from '../cardapio2/cardapio2';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Cardapio2Page } from "../cardapio2/cardapio2";
 
 /**
  * Generated class for the CardapioListPage page.
@@ -11,80 +11,66 @@ import { Cardapio2Page } from '../cardapio2/cardapio2';
 
 @IonicPage()
 @Component({
-  selector: 'page-cardapio-list',
-  templateUrl: 'cardapio-list.html',
+  selector: "page-cardapio-list",
+  templateUrl: "cardapio-list.html"
 })
 export class CardapioListPage {
-
   selectedItem: any = "comidas";
   myLocation: any;
 
-  searchQuery: string = '';
-
+  searchQuery: string = "";
+  private comidas = [];
+  private bebidas = [];
+  private sobremesas = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // this.geolocation.getCurrentPosition().then((resp) => {
-    //   console.log(resp.coords.latitude);
-    //   console.log(resp.coords.longitude);
-    // }).catch((error) => {
-    //   console.log('Error getting location', error);
-    // });
-
-    // let watch = this.geolocation.watchPosition();
-    // watch.subscribe((data) => {
-    //   // data can be a set of coordinates, or an error (if an error occurred).
-    //   // data.coords.latitude
-    //   // data.coords.longitude
-    // });
-
+    this.initializeItems();
   }
 
-  private comidas = [
-    { nome: "Lasanha", urlImage: "http://www.pifpaf.com.br/img/000000000000050138006.JPG", valor: 10 },
-    { nome: "Comida 2", urlImage: "xxx", valor: 10 },
-    { nome: "Comida 3", urlImage: "xxx", valor: 10 },
-    { nome: "Comida 4", urlImage: "xxx", valor: 10 },
-    { nome: "Comida 5", urlImage: "xxx", valor: 10 }
-  ];
-
-  private bebidas = [
-    { nome: "bebidas 1", urlImage: "xxx", valor: 10 },
-    { nome: "bebidas2", urlImage: "xxx", valor: 10 },
-    { nome: "bebidas3", urlImage: "xxx", valor: 10 },
-    { nome: "bebidas4", urlImage: "xxx", valor: 10 },
-    { nome: "bebidas5", urlImage: "xxx", valor: 10 }
-  ];
-
-  private sobremesas = [
-    { nome: "sobremesas1", urlImage: "xxx", valor: 10 },
-    { nome: "sobremesas2", urlImage: "xxx", valor: 10 },
-    { nome: "sobremesas3", urlImage: "xxx", valor: 10 },
-    { nome: "sobremesas4", urlImage: "xxx", valor: 10 },
-    { nome: "sobremesas5", urlImage: "xxx", valor: 10 }
-  ];
+  initializeItems() {
+    this.bebidas = [
+      { nome: "bebidas 1", urlImage: "xxx", valor: 10 },
+      { nome: "bebidas2", urlImage: "xxx", valor: 10 },
+      { nome: "bebidas3", urlImage: "xxx", valor: 10 },
+      { nome: "bebidas4", urlImage: "xxx", valor: 10 },
+      { nome: "bebidas5", urlImage: "xxx", valor: 10 }
+    ];
+    this.comidas = [
+      {
+        nome: "Lasanha",
+        urlImage: "http://www.pifpaf.com.br/img/000000000000050138006.JPG",
+        valor: 10
+      },
+      { nome: "Comida 2", urlImage: "xxx", valor: 10 },
+      { nome: "Comida 3", urlImage: "xxx", valor: 10 },
+      { nome: "Comida 4", urlImage: "xxx", valor: 10 },
+      { nome: "Comida 5", urlImage: "xxx", valor: 10 }
+    ];
+    this.sobremesas = [
+      { nome: "sobremesas1", urlImage: "xxx", valor: 10 },
+      { nome: "sobremesas2", urlImage: "xxx", valor: 10 },
+      { nome: "sobremesas3", urlImage: "xxx", valor: 10 },
+      { nome: "sobremesas4", urlImage: "xxx", valor: 10 },
+      { nome: "sobremesas5", urlImage: "xxx", valor: 10 }
+    ];
+  }
 
   getItems(ev: any) {
+    this.initializeItems();
     let val = ev.target.value;
-    if (val && val.trim() != '') {
-      this.comidas = this.comidas.filter((item) => {
-        return (item.nome.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-      this.sobremesas = this.sobremesas.filter((item) => {
-        return (item.nome.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-      this.bebidas = this.bebidas.filter((item)=> {
-        return (item.nome.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
+    if (val && val.trim() != "") {
+      this.comidas = this.comidas.filter(item => {
+        return item.nome.toLowerCase().indexOf(val.toLowerCase()) > -1;
+      });
     }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CardapioListPage');
+    console.log("ionViewDidLoad CardapioListPage");
   }
 
   moveTo(item: any) {
     console.log(item);
     this.navCtrl.push(Cardapio2Page, item);
   }
-
 }
