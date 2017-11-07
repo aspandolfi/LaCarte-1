4cbea5f04d70b53f6548ce659fe7c09092d9f93d
lukasg18 <lukas.gomes@hotmail.com>
2017-11-07 19:41:46 -0200

    acertando app no iphone

--- src/pages/cardapio-list/cardapio-list.ts
+++ src/pages/cardapio-list/cardapio-list.ts
@@ -30,27 +30,79 @@ export class CardapioListPage {
 
   initializeItems() {
     this.produtos = [
-      { nome: "Lasanha", urlImage: "http://www.pifpaf.com.br/img/000000000000050138006.JPG", valor: 10, tipo: 1},
-      { nome: "Hamburguer", urlImage: "https://www.designmaster.com.br/designmarketing/produtos/g_foto1_3246.jpg", valor: 10, tipo: 1},
-      { nome: "Macarrão", urlImage: "https://s3.amazonaws.com/midia.naminhapanela.com/wp-content/uploads/2016/08/14134251/amatriciana4.png", valor: 10, tipo: 1},
-      { nome: "Agua", urlImage: "http://www.flaska.com.br/documents/flaska.com.br/large/396.jpg", valor: 10, tipo: 2},
-      { nome: "Refrigerante", urlImage: "http://midias.gazetaonline.com.br/_midias/jpg/2017/06/20/refrigerante-5158046.jpg", valor: 10, tipo: 2},
-      { nome: "Suco de laranja", urlImage: "http://www.xando.com.br/images/itens/itemCopo.jpg", valor: 10, tipo: 2},
-      { nome: "Sorvete de chocolate", urlImage: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia11042/slide/sorvetes1-cursos-cpt.jpg", valor: 10, tipo: 3},
-      { nome: "Mousse de morango", urlImage: "https://www.embare.com.br/wp-content/uploads/2013/06/mousse-de-morango-receitas-embare.jpg", valor: 10, tipo: 3},
-      { nome: "Torta de limão", urlImage: "http://www.corpoealma.com/wp-content/uploads/2016/08/Receita-de-Torta-de-Lima%CC%83o-2.jpg", valor: 10, tipo: 3},
+      {
+        nome: "Lasanha",
+        urlImage: "http://www.pifpaf.com.br/img/000000000000050138006.JPG",
+        valor: 10,
+        tipo: 1
+      },
+      {
+        nome: "Hamburguer",
+        urlImage:
+          "https://www.designmaster.com.br/designmarketing/produtos/g_foto1_3246.jpg",
+        valor: 10,
+        tipo: 1
+      },
+      {
+        nome: "Macarrão",
+        urlImage:
+          "https://s3.amazonaws.com/midia.naminhapanela.com/wp-content/uploads/2016/08/14134251/amatriciana4.png",
+        valor: 10,
+        tipo: 1
+      },
+      {
+        nome: "Agua",
+        urlImage:
+          "http://www.flaska.com.br/documents/flaska.com.br/large/396.jpg",
+        valor: 10,
+        tipo: 2
+      },
+      {
+        nome: "Refrigerante",
+        urlImage:
+          "http://midias.gazetaonline.com.br/_midias/jpg/2017/06/20/refrigerante-5158046.jpg",
+        valor: 10,
+        tipo: 2
+      },
+      {
+        nome: "Suco de laranja",
+        urlImage: "http://www.xando.com.br/images/itens/itemCopo.jpg",
+        valor: 10,
+        tipo: 2
+      },
+      {
+        nome: "Sorvete de chocolate",
+        urlImage:
+          "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia11042/slide/sorvetes1-cursos-cpt.jpg",
+        valor: 10,
+        tipo: 3
+      },
+      {
+        nome: "Mousse de morango",
+        urlImage:
+          "https://www.embare.com.br/wp-content/uploads/2013/06/mousse-de-morango-receitas-embare.jpg",
+        valor: 10,
+        tipo: 3
+      },
+      {
+        nome: "Torta de limão",
+        urlImage:
+          "http://www.corpoealma.com/wp-content/uploads/2016/08/Receita-de-Torta-de-Lima%CC%83o-2.jpg",
+        valor: 10,
+        tipo: 3
+      }
     ];
     this.separarCat();
   }
 
-  separarCat(){
+  separarCat() {
     this.comidas = [];
     this.bebidas = [];
     this.sobremesas = [];
-    for(let i = 0; i < this.produtos.length; i++){
-      if(this.produtos[i].tipo==1) this.comidas.push(this.produtos[i]);
-      if(this.produtos[i].tipo==2) this.bebidas.push(this.produtos[i]);
-      if(this.produtos[i].tipo==3) this.sobremesas.push(this.produtos[i]);
+    for (let i = 0; i < this.produtos.length; i++) {
+      if (this.produtos[i].tipo == 1) this.comidas.push(this.produtos[i]);
+      if (this.produtos[i].tipo == 2) this.bebidas.push(this.produtos[i]);
+      if (this.produtos[i].tipo == 3) this.sobremesas.push(this.produtos[i]);
     }
   }
 
@@ -58,10 +110,12 @@ export class CardapioListPage {
     this.initializeItems();
     let val = ev.target.value;
     if (val && val.trim() != "") {
-      this.comidas = this.bebidas = this.sobremesas = this.produtos.filter(item => {
-        return item.nome.toLowerCase().indexOf(val.toLowerCase()) > -1;
-      });
-    }else{
+      this.comidas = this.bebidas = this.sobremesas = this.produtos.filter(
+        item => {
+          return item.nome.toLowerCase().indexOf(val.toLowerCase()) > -1;
+        }
+      );
+    } else {
       this.separarCat();
     }
   }
