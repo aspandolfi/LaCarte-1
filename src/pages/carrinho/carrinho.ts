7f20fdba6b9ccfcbcbbb080b26d7b5ab9714c825
lukasg18 <lukas.gomes@hotmail.com>
2017-11-20 21:27:54 -0200

    tentando fazer o botão remover funcionar em detalhes

--- src/pages/carrinho/carrinho.ts
+++ src/pages/carrinho/carrinho.ts
@@ -1,15 +1,20 @@
-import { DetalhePedidoPage } from './../detalhe-pedido/detalhe-pedido';
 //Modulos
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams, AlertController } from "ionic-angular";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController
+} from "ionic-angular";
 import { Storage } from "@ionic/storage";
+import { Events } from "ionic-angular";
 //Paginas
 import { ComandaPage } from "./../comanda/comanda";
 import { CardapioListPage } from "./../cardapio-list/cardapio-list";
+import { DetalhePedidoPage } from "./../detalhe-pedido/detalhe-pedido";
 //Classes
 import { ItemPedido } from "../../class/ItemPedido";
 import { Comanda } from "../../class/ItemComanda";
-
 
 @IonicPage()
 @Component({
@@ -25,28 +30,33 @@ export class CarrinhoPage {
     public navParams: NavParams,
     public storage: Storage,
     public alertCtrl: AlertController,
+    public events: Events
   ) {
-    if(navParams.data!=={}){
-      console.log("ENTROU");
-      console.log(navParams.data.id);
-      this.carrinho.push(navParams.data);
+    //if(navParams.data instanceof ItemPedido){}
+    this.events.subscribe('reloadCarrinhoPage',() => {
+      this.carrinho = []
       this.loadCarrinho();
+    });
+    if (Object.keys(navParams.data).length !== 0) {
+      this.carrinho.push(navParams.data);
     }
+    this.loadCarrinho();
   }
 
-  loadCarrinho() {
-    this.storage.get("carrinho")
-      .then((data: Array<ItemPedido>) => {
-        let id = 1;
-        if (data) { // Se já tem conteudo
-          this.carrinho = this.carrinho.concat(data);
-          if (data.length != 0) id = data[0].id + 1;
-        }
+  async loadCarrinho() {
+    this.storage.get("carrinho").then((data: Array<ItemPedido>) => {
+      let id = 1;
+      if (data) {
+        // Se já tem conteudo
+        this.carrinho = this.carrinho.concat(data);
+        if (data.length > 0) id = data[0].id + 1;
+      }
+      if (this.carrinho.length > 0) {
         this.carrinho[0].id = id; // TODO: pegar id pronto do banco
         this.storage.set("carrinho", this.carrinho);
         this.loadComanda();
       }
-      );
+    });
   }
 
   loadComanda() {
@@ -56,19 +66,28 @@ export class CarrinhoPage {
       this.comanda.pedido.push(this.carrinho[i]);
       this.comanda.id = 1;
     }
-    this.storage.get("mesa").then((data) => {
+    this.storage.get("mesa").then(data => {
       this.comanda.mesa = data;
     });
   }
 
-  saveComanda(){
-    this.storage.get("comanda")
-    .then((data: Comanda) => {
-      if (data) { // Se já tem conteudo
+  saveComanda() {
+    this.storage.get("comanda").then((data: Comanda) => {
+      if (data) {
+        // Se já tem conteudo
         this.comanda.pedido = this.comanda.pedido.concat(data.pedido);
       }
       this.storage.set("comanda", this.comanda);
     });
+  }
+
+  public getItemById(id:number):ItemPedido{
+    for(let i=0; i < this.carrinho.length; i++){
+      if(this.carrinho[i].id === id){
+        return this.carrinho[i];
+      }
+    }
+    return null;
   }
 
   public addItem() {
@@ -79,7 +98,7 @@ export class CarrinhoPage {
     this.carrinho = this.carrinho.filter(itemNaLista => {
       return itemNaLista.id !== item.id;
     });
-    this.storage.set("carrinho",this.carrinho);
+    this.storage.set("carrinho", this.carrinho);
     this.loadComanda();
   }
 
--- src/pages/detalhe-pedido/detalhe-pedido.ts
+++ src/pages/detalhe-pedido/detalhe-pedido.ts
@@ -1,10 +1,12 @@
-import { CarrinhoPage } from './../carrinho/carrinho';
-import { ItemPedido } from './../../class/ItemPedido';
-import { Produto } from './../../class/produtos';
+import { CarrinhoPage } from "./../carrinho/carrinho";
+import { ItemPedido } from "./../../class/ItemPedido";
+import { Produto } from "./../../class/produtos";
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { Storage } from "@ionic/storage";
 import { AlertController } from "ionic-angular";
+import { Comanda } from "../../class/ItemComanda";
+import { Events } from "ionic-angular";
 
 /**
  * Generated class for the DetalhePedidoPage page.
@@ -23,19 +25,84 @@ export class DetalhePedidoPage {
   public itemPedido = new ItemPedido();
   public txtAdicio = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public alertCtrl: AlertController) {
+  public carrinho: Array<ItemPedido> = [];
+  public comanda: Comanda;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: Storage,
+    public alertCtrl: AlertController,
+    public events: Events
+  ) {
     this.itemPedido = navParams.data;
+    this.loadCarrinho();
   }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad DetalhePedidoPage");
   }
 
+  async loadCarrinho() {
+    this.storage.get("carrinho").then((data: Array<ItemPedido>) => {
+      let id = 1;
+      if (data) {
+        // Se já tem conteudo
+        this.carrinho = this.carrinho.concat(data);
+        if (data.length > 0) id = data[0].id + 1;
+      }
+      if (this.carrinho.length > 0) {
+        this.carrinho[0].id = id; // TODO: pegar id pronto do banco
+        this.storage.set("carrinho", this.carrinho);
+        this.loadComanda();
+      }
+    });
+  }
+
+  loadComanda() {
+    this.comanda = new Comanda();
+    this.comanda.pedido = new Array<ItemPedido>();
+    for (let i = 0; i < this.carrinho.length; i++) {
+      this.comanda.pedido.push(this.carrinho[i]);
+      this.comanda.id = 1;
+    }
+    this.storage.get("mesa").then(data => {
+      this.comanda.mesa = data;
+    });
+  }
+
+  saveCarrinho() {
+    this.storage.set("carrinho", this.carrinho);
+  }
+
+  saveComanda() {
+    this.storage.get("comanda").then((data: Comanda) => {
+      if (data) {
+        // Se já tem conteudo
+        this.comanda.pedido = this.comanda.pedido.concat(data.pedido);
+      }
+      this.storage.set("comanda", this.comanda);
+    });
+  }
+
+  public removeItem(item: any) {
+    console.log("ANTES");
+    console.log(this.carrinho);
+    this.carrinho = this.carrinho.filter(itemNaLista => {
+      console.log("itemNaLista.id !== item.id");
+      console.log(itemNaLista.id,"!==",item.id);
+      return itemNaLista.id !== item.id;
+    });
+    console.log("DEPOIS");
+    console.log(this.carrinho);
+    this.storage.set("carrinho", this.carrinho);
+    this.loadComanda();
+  }
+
   showConfirm() {
     let confirm = this.alertCtrl.create({
       title: "Cancelamento",
-      message:
-        "Deseja cancelar o pedido?",
+      message: "Deseja cancelar o pedido?",
       buttons: [
         {
           text: "Não",
@@ -46,7 +113,9 @@ export class DetalhePedidoPage {
         {
           text: "Sim",
           handler: () => {
-            this.navCtrl.setRoot(CarrinhoPage, this.itemPedido);
+            this.removeItem(this.itemPedido);
+            this.events.publish('reloadCarrinhoPage');
+            this.navCtrl.pop();
           }
         }
       ]
