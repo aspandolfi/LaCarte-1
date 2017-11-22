fef1778ea5a21cba88353c4e1590982c5aa2f3f3
luizlk <luiz.lk.lima@gmail.com>
2017-11-22 20:56:01 -0200

    status do pedido funcionando

--- src/pages/cozinha-detalhe/cozinha-detalhe.ts
+++ src/pages/cozinha-detalhe/cozinha-detalhe.ts
@@ -1,7 +1,7 @@
 import { ItemPedido } from "./../../class/ItemPedido";
 import { Produto } from "./../../class/produtos";
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, Events } from "ionic-angular";
 import { Storage } from "@ionic/storage";
 import { AlertController } from "ionic-angular";
 
@@ -12,16 +12,21 @@ import { AlertController } from "ionic-angular";
   templateUrl: "cozinha-detalhe.html"
 })
 export class CozinhaDetalhePage {
-  testRadioResult: any;
-  testRadioOpen: boolean;
-  itemPedido: ItemPedido;
+  public itemPedido: ItemPedido;
+  public txtAdicio = "";
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: Storage,
-    public alertCtrl: AlertController
+    public alertCtrl: AlertController,
+    public events: Events
   ) {
+    this.itemPedido = navParams.data;
+
+    if (this.itemPedido.produto.adicional.length > 0) {
+      this.txtAdicio = "Adicionais";
+    }
   }
 
   ionViewDidLoad() {
@@ -30,41 +35,15 @@ export class CozinhaDetalhePage {
   recusar() {
     let alert = this.alertCtrl.create();
     alert.setTitle("Motivo do cancelamento");
-
-    alert.addInput({
-      type: "radio",
-      label: "Produto incompleto",
-      value: "1",
-      checked: true
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "Adicional em falta",
-      value: "2",
-      checked: false
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "Equipamento para o preparo com defeito",
-      value: "3",
-      checked: false
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "Outro",
-      value: "4",
-      checked: false
-    });
-
+    alert.addInput({ type: "radio", label: "Produto incompleto", value: "Produto incompleto", checked: true });
+    alert.addInput({ type: "radio", label: "Adicional em falta", value: "Adicional em falta", checked: false });
+    alert.addInput({ type: "radio", label: "Equipamento para o preparo com defeito", value: "Equipamento para o preparo com defeito", checked: false });
+    alert.addInput({ type: "radio", label: "Outro", value: "Outro", checked: false });
     alert.addButton("Cancel");
     alert.addButton({
       text: "OK",
       handler: data => {
-        this.testRadioOpen = false;
-        this.testRadioResult = data;
+        this.moveTo(2, data); //2 eh status de cancelado
       }
     });
     alert.present();
@@ -73,57 +52,25 @@ export class CozinhaDetalhePage {
   confirmar() {
     let alert = this.alertCtrl.create();
     alert.setTitle("Tempo Estimado");
-
-    alert.addInput({
-      type: "radio",
-      label: "10",
-      value: "1",
-      checked: true
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "20",
-      value: "2",
-      checked: false
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "30",
-      value: "3",
-      checked: false
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "40",
-      value: "4",
-      checked: false
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "50",
-      value: "4",
-      checked: false
-    });
-
-    alert.addInput({
-      type: "radio",
-      label: "60",
-      value: "4",
-      checked: false
-    });
-
+    alert.addInput({ type: "radio", label: "10", value: "10", checked: true });
+    alert.addInput({ type: "radio", label: "20", value: "20", checked: false });
+    alert.addInput({ type: "radio", label: "30", value: "30", checked: false });
+    alert.addInput({ type: "radio", label: "40", value: "40", checked: false });
+    alert.addInput({ type: "radio", label: "50", value: "50", checked: false });
+    alert.addInput({ type: "radio", label: "60", value: "60", checked: false });
     alert.addButton("Cancel");
     alert.addButton({
       text: "OK",
       handler: data => {
-        this.testRadioOpen = false;
-        this.testRadioResult = data;
+        this.moveTo(1, data); //1 eh status de aceito
       }
     });
     alert.present();
+  }
+
+  moveTo(op: number, val: any) {
+    let objeto = { val: op, id: this.itemPedido.id };
+    this.events.publish('atualizarItemStatus', objeto);
+    this.navCtrl.pop();
   }
 }
